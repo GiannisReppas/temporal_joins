4a4890ac4f08403055f6e782607e9f0fe91934c5
Giannis Reppas <giannisreppas@hotmail.com>
2024-08-30 10:12:53 +0300

    old DIP

--- algorithms/dip_anti.cpp
+++ algorithms/dip_anti.cpp
@@ -70,102 +70,129 @@ void create_dip(Relation& R, std::vector<dip_heap_node>& heap_r)
 
 uint64_t dip_merge( std::vector<dip_heap_node>& heap_r, Relation& S, Timestamp& domainStart, Timestamp& domainEnd)
 {
-	uint64_t result = 0;
+	// DIPmerge variables and result (We consider that null timepoint is +INFINITY)
+	unsigned long long result = 0;
+	const Timestamp null_timepoint = (0 - 1);
+	const Timestamp m = heap_r.size();
 
-	// lead variables
+	// load r
+	Record** current_r = (Record**) malloc( m*sizeof(Record*) );
+	Record** end_r = (Record**) malloc( m*sizeof(Record*) );
+	Record* r = (Record*) malloc( m*sizeof(Record) );
+	bool* r_nulls = (bool*) malloc( m*sizeof(bool) );
+	for (uint32_t i = 0; i < m; i++)
+	{
+		current_r[i] = &heap_r[i].partition[0];
+		end_r[i] = &heap_r[i].partition[0] + heap_r[i].partition.size();
+		r_nulls[i] = false;
+		// fetchRow(R_i)
+		r[i] = *(current_r[i])++;
+	}
+
+	// load s
+	// DIP considers domainStart = -INFINITY, so we need some extra cleaning before main loop
+	Record* current_s = S.record_list;
+	const Record* end_s = S.record_list + S.numRecords;
+	std::pair<Record,Record> s;
+	bool s_null = false;
+	// fetchRow(S)
+	s.first = *current_s++;
+	s.second = Record( domainStart, s.first.start );
 	Timestamp longestS = domainStart;
-	Timestamp leadStart, leadEnd;
-
-	// initialize current and end position for pointers in heap_r
-	size_t partitions_num = heap_r.size();
-	Record** currentR = (Record**) malloc( partitions_num * sizeof(Record*) );
-	Record** endR = (Record**) malloc( partitions_num * sizeof(Record*) );
-	for (uint32_t i = 0; i < partitions_num; i++)
+	while (s.second.start >= s.second.end)
 	{
-		currentR[i] = &heap_r[i].partition[0];
-		endR[i] = &heap_r[i].partition[0] + heap_r[i].partition.size();
+		// fetchRow(S)
+		if (current_s == end_s)
+			s_null = true;
+		else
+			s.first = *current_s++;;
+		longestS = std::max( (current_s-2)->end, longestS);
+		if (s_null)
+		{
+			longestS = std::max( (current_s-1)->end, longestS);
+			s.first.start = null_timepoint;
+			if (longestS == domainEnd)
+				return 0;
+			else
+				s.second = Record( longestS, domainEnd);
+			break;
+		}
+		else
+		{
+			s.second = Record( longestS, (current_s-1)->start);
+		}
 	}
 
-	// scan S and get s.X in every step of the loop
-	Record* currentS = S.record_list;
-	Record* lastS = S.record_list + S.numRecords;
-	while (currentS != lastS)
+	// main loop
+	Timestamp i = 0;
+	while ( (r[i].start != null_timepoint) || (s.first.start != null_timepoint) )
 	{
-		// get s.X, scan partitions for overlaps, if its > 0
-		if (longestS < currentS->start)
+		if ( (s.second.start < s.second.end) && ( (r[i].start < s.second.end) && (s.second.start < r[i].end) ) ) // overlap check
+				result++;
+
+		if ( (r[i].start != null_timepoint) && ( (s.first.start == null_timepoint) || (r[i].end <= s.first.end) ) )
 		{
-			leadStart = longestS;
-			leadEnd = currentS->start;
-			longestS = currentS->end;
+			// fetchRow(R)
+			if (current_r[i] == end_r[i])
+				r_nulls[i] = true;
+			else
+				r[i] = *(current_r[i])++;
 
-			// scan DIP partitions for s.X
-			for (uint32_t i = 0; i < partitions_num; i++)
+			if (r_nulls[i])
+				r[i].start = null_timepoint;
+		}
+		else
+		{
+			if (i < (m-1))
 			{
-				while (currentR[i] != endR[i])
-				{
-					// 3 cases:
-					// a) overlap -> output result tuple and move to next tuple from R
-					// b) no overlap and move to next tuple from R (r is left of S.X)
-					// c) no overlap and break (r is right of S.X);
-					if ( currentR[i]->start >= leadEnd ) // case b
-					{
-						break;
-					}
-					else if ( currentR[i]->end > leadStart ) // case a
-					{
-#ifdef WORKLOAD_COUNT
-						result += 1;
-#else
-						result += currentR[i]->start ^ leadStart;
-#endif
-					}
-
-					currentR[i]++;
-				}
-				if (currentR[i] != &heap_r[i].partition[0])
-					currentR[i]--;
+				i++;
 			}
-		}
-		else if (longestS < currentS->end)
-		{
-			longestS = currentS->end;
-		}
-
-		currentS++;
-	}
-
-	// one last step for last lead with domainEnd (doesnt exist like that in original DIP but its part of complement design)
-	currentS--;
-	if ( longestS < domainEnd)
-	{
-		leadStart = longestS;
-		leadEnd = domainEnd;
-
-		for (uint32_t i = 0; i < partitions_num; i++)
-		{
-			while (currentR[i] != endR[i])
+			else
 			{
-				if ( currentR[i]->start >= leadEnd ) // case b
-				{
-					break;
-				}
-				else if ( currentR[i]->end > leadStart ) // case a
-				{
-#ifdef WORKLOAD_COUNT
-					result += 1;
-#else
-					result += currentR[i]->start ^ leadStart;
-#endif
-				}
+				i = 0;
 
-				currentR[i]++;
+				if (s.first.end > s.second.start)
+					longestS = s.first.end;
+
+				// fetchRow(S)
+				if (current_s == end_s)
+					s_null = true;
+				else
+					s.first = *current_s++;
+
+				if (!s_null)
+				{
+					s.second = Record(longestS, s.first.start);
+				}
+				else
+				{
+					s.first.start = null_timepoint;
+					s.second = Record(longestS, domainEnd);
+				}
 			}
 		}
 	}
 
-	// return result
-	free( currentR );
-	free( endR );
+	// last step for last lead
+	for (Timestamp i = 0; i < m; i++)
+	{
+		if (r[i].start != null_timepoint)
+		{
+			for (Record* j = current_r[i]-1; j != end_r[i]; j++)
+			{
+				if (s.second.start < s.second.end)
+					if ( (j->start < s.second.end) && (s.second.start < j->end) && (r[i].start != null_timepoint) && (s.second.end != null_timepoint) )
+						result++;
+			}
+		}
+	}
+
+	// free and return result
+	free(r_nulls);
+	free(r);
+	free(current_r);
+	free(end_r);
+
 	return result;
 }
 
